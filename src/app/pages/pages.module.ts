5a36eedbf8e5b041db9b336b95ba98daddc35287
Daniel Veloza <ciberronaldo@hotmail.com>
2023-05-25 19:52:02 -0500

    feat: creación del banner de la pagina de Home y se agregaron assets

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { CoreModule } from '../core/core.module';
+import { BannerComponent } from './home/banner/banner.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    BannerComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    CoreModule
   ]
 })
 export class PagesModule { }
